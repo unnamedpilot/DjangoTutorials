0972a2d64e62dac8ea98b082029012726b40176f
Tu nombre <alverth.antonio@hotmail.com>
2024-02-27 15:38:42 -0500

    Tutorial 03

--- helloworld/helloworld_project/pages/urls.py
+++ helloworld/helloworld_project/pages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+
+from .utils import ImageLocalStorage
 from .views import (HomePageView, AboutPageView, contact_page,
-                    ProductIndexView, ProductShowView, ProductCreateView)
+                    ProductIndexView, ProductShowView, ProductCreateView, CartView, CartRemoveAllView, ImageViewFactory,
+                    ImageViewNoDI)
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
@@ -8,4 +11,12 @@ urlpatterns = [
     path('contact/', contact_page, name='contact'),
     path('products/', ProductIndexView.as_view(), name='index'),
     path('products/create', ProductCreateView.as_view(), name='form'),
-    path('products/<str:id>', ProductShowView.as_view(), name='show')]
+    path('products/<str:id>', ProductShowView.as_view(), name='show'),
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+    #path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+    #path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
+    path('imagenotdi/', ImageViewNoDI.as_view(), name='imagenodi_index'),
+    path('image/save', ImageViewNoDI.as_view().as_view(), name='imagenodi_save'),
+]
